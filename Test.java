afd1177d569e35a84b5b4d59a3b82ed77b78afc0
balram225726 <63298001+balram225726@users.noreply.github.com>
2020-04-07 18:39:55 +0530

    second commit command is done now

--- Test.java
+++ Test.java
@@ -2,4 +2,5 @@ class Test{
 
   public static  void main(String args[]){
   System.out.println("this is my first github project setting in sts tool for practive only");
+    System.out.println("This is second time commit commond is goind on");
 }
